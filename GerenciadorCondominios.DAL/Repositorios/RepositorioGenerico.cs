6b2055420eb482d6ce650b09e9f664fe1830ee7d
narba <narba@DESKTOP-KRRRREC>
2022-10-18 18:32:38 -0300

    Interface e Repository

---
+++ GerenciadorCondominios.DAL/Repositorios/RepositorioGenerico.cs
@@ -0,0 +1,127 @@
+﻿using GerenciadorCondominios.DAL.Context;
+using GerenciadorCondominios.DAL.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace GerenciadorCondominios.DAL.Repositorios
+{
+    public class RepositorioGenerico<TEntity> : IRepositorioGenerico<TEntity> where TEntity : class
+    {
+        private readonly DataContext _contexto;
+        public RepositorioGenerico(DataContext context)
+        {
+            _contexto = context;
+        }
+        public async Task Atualizar(TEntity entity)
+        {
+            try
+            {
+                _contexto.Set<TEntity>().Update(entity);
+                await _contexto.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public async Task Exlcuir(TEntity entity)
+        {
+            try
+            {
+                _contexto.Set<TEntity>().Remove(entity);
+                await _contexto.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public async Task Exlcuir(int id)
+        {
+            try
+            {
+                var entity = await PegarPeloId(id);
+                _contexto.Set<TEntity>().Remove(entity);
+                await _contexto.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public async Task Exlcuir(string id)
+        {
+
+            try
+            {
+                var entity = await PegarPeloId(id);
+                _contexto.Set<TEntity>().Remove(entity);
+                await _contexto.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public async Task Inserir(TEntity entity)
+        {
+
+            try
+            {
+                await _contexto.AddAsync(entity);
+                await _contexto.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public async Task<TEntity> PegarPeloId(int id)
+        {
+            try
+            {
+                return await _contexto.Set<TEntity>().FindAsync(id);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public async Task<TEntity> PegarPeloId(string id)
+        {
+            try
+            {
+                return await _contexto.Set<TEntity>().FindAsync(id);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public async Task<IEnumerable<TEntity>> PegarTodos()
+        {
+            try
+            {
+                return await _contexto.Set<TEntity>().ToListAsync();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
---
+++ GerenciadorCondominios/ViewModels/ResgitroViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GerenciadorCondominios.ViewModels
+{
+    public class ResgitroViewModel
+    {
+        [Required(ErrorMessage = "O Campo {0} é obrigatório")]
+        [StringLength(40, ErrorMessage = "Use menos caracteres")]
+        public string Nome { get; set; }
+
+
+        [Required(ErrorMessage = "O Campo {0} é obrigatório")]
+        public string CPF { get; set; }
+
+        [Required(ErrorMessage = "O Campo {0} é obrigatório")]
+        public string Telefone { get; set; }
+        public string Foto { get; set; }
+
+
+
+        [Required(ErrorMessage = "O Campo {0} é obrigatório")]
+        [StringLength(40, ErrorMessage = "Use menos caracteres")]
+        [EmailAddress(ErrorMessage = "Email inválido")]
+        public string Email { get; set; }
+
+
+
+        [Required(ErrorMessage = "O Campo {0} é obrigatório")]
+        [StringLength(40, ErrorMessage = "Use menos caracteres")]
+        [DataType(DataType.Password)]
+        public string Senha { get; set; }
+
+        [Required(ErrorMessage = "O Campo {0} é obrigatório")]
+        [StringLength(40, ErrorMessage = "Use menos caracteres")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirme sua senha")]
+        [Compare("Senha", ErrorMessage = "As senhas não conferem")]
+        public string SenhaConfirmada { get; set; }
+    }
+}
